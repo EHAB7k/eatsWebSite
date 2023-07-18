f459c99dcc53083f7db107555a407804263f4c05
Ehab hakami <ehab7k@gmail.com>
2023-07-18 18:18:40 +0300

    lastUapdate18jul

--- src/components/MassageAndVi.js
+++ src/components/MassageAndVi.js
@@ -6,12 +6,12 @@ function MassageAndVi() {
       <div dir="rtl" className="gap-5  flex flex-row justify-center">
         <div
           href="#"
-          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-dark-300 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-dark-300 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <mark class=" block p-2 bg-yellow-300 dark:bg-yellow-300 text-center text-lg  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+          <mark className=" block p-2 bg-yellow-300 dark:bg-yellow-300 text-center text-lg  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
             رسالتنا
           </mark>
-          <p class="font-normal text-gray-700 dark:text-gray-400 m-auto mt-5">
+          <p className="font-normal text-gray-700 dark:text-gray-400 m-auto mt-5">
             استخدام الموارد البشرية والمالية والتقنية وبناء الخطط الاستراتيجية
             والتسويقية لنجعل التطبيق الوجهة الأولى للعملاء
           </p>
@@ -19,12 +19,12 @@ function MassageAndVi() {
 
         <div
           href="#"
-          class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-dark-300 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-dark-300 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <mark class=" block p-2 bg-yellow-300 dark:bg-yellow-300 text-center text-lg  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+          <mark className=" block p-2 bg-yellow-300 dark:bg-yellow-300 text-center text-lg  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
             رؤيتنا
           </mark>
-          <p class="font-normal text-gray-700 dark:text-gray-400 m-auto">
+          <p className="font-normal text-gray-700 dark:text-gray-400 m-auto">
             أن تكون شركة توصيل الخليجية صانعة للحلول والاستثمار بها من خلال
             التقنيات الحديثة التي من شأنها أن تقود نشاط الخدمات في قطاع الأغذية
             والمشروبات في المملكة العربية السعودية والخليج وتوفر أبرز
