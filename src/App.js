6fbbd929143439e0d2caa8fb169bb4d75ea9e860
Ehab hakami <ehab7k@gmail.com>
2023-08-06 16:06:17 +0300

    debloy2

--- src/App.js
+++ src/App.js
@@ -1,64 +1,49 @@
-import './App.css';
-import {useEffect} from 'react'
-import Navbar from './components/Shared/Navbar';
+import './App.css'
+import { useEffect } from 'react'
+import Navbar from './components/Shared/Navbar'
 // import Hiro from './components/hiro/Hiro';
 import Skills from './components/Skills'
-import Honors from './components/Honors';
-import Certs from './components/Certs';
-import Footer from './components/Footer';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import CardApp from './components/CardApp';
-import {BrowserRouter, Route, Router, Switch} from "react-router-dom"
-import Inventory from './components/inventory/Inventory';
-import Home from './components/home/Home';
-import { createBrowserHistory } from "history";
-import {useState} from 'react'
-const history = createBrowserHistory();
+import Honors from './components/Honors'
+import Certs from './components/Certs'
+import Footer from './components/Footer'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import CardApp from './components/CardApp'
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom'
+import Inventory from './components/inventory/Inventory'
+import Home from './components/home/Home'
+import { createBrowserHistory } from 'history'
+import { useState } from 'react'
+const history = createBrowserHistory()
 function App() {
-    useEffect(() => {
-      document.title = 'Eats';
-      AOS.init();
-    }, []);
+  useEffect(() => {
+    document.title = 'Eats'
+    AOS.init()
+  }, [])
 
-    
   return (
     <>
-     
-    {/* <div className='px-10 lg:px-0 xl:px-36'> */}
+      {/* <div className='px-10 lg:px-0 xl:px-36'> */}
 
-      
-       <div className=" dark:bg-dark-500">
-      
-      <Navbar/>
-      {/* <Hiro />
+      <div className=" dark:bg-dark-500">
+        <Navbar />
+        {/* <Hiro />
       <br/>
       <CardApp/>
       <Skills />
       {/* <Honors /> */}
-      {/* <Certs /> */} 
-      
-      
+        {/* <Certs /> */}
+
         <Switch>
+          <Route path="/" exact component={Home} />
 
-      <Route path="/" exact component={Home} />
-        
-      <Route path="/inventory" component={Inventory} />
-      
-      </Switch>
-      
-      
-   
-    
+          <Route path="/inventory" component={Inventory} />
+        </Switch>
 
-      <Footer />
+        <Footer />
       </div>
-       
-      
-    
-
     </>
-  );
+  )
 }
 
-export default App;
+export default App
