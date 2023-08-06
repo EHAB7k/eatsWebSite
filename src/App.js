ab8a5fc462a888fed7febc0c6aa1b30274b2d3f5
Ehab hakami <ehab7k@gmail.com>
2023-08-06 17:35:29 +0300

    testAb2 deploy

--- src/App.js
+++ src/App.js
@@ -14,26 +14,58 @@ import Inventory from './components/inventory/Inventory'
 import Home from './components/home/Home'
 import { createBrowserHistory } from 'history'
 import { useState } from 'react'
+import Hiro from './components/Hiro'
+import MassageAndVi from './components/MassageAndVi'
+import PepoleMangament from './components/PepoleMangament'
+import AboutHealth from './components/AboutHealth'
+import AsNavFor from './components/AsNavFor'
+import OurAdvantages from './components/OurAdvantages'
+import SlowerExample from './components/SlowerExample'
+import ContactForm from './components/ContactForm'
 const history = createBrowserHistory()
 function App() {
   useEffect(() => {
     document.title = 'Eats'
     AOS.init()
   }, [])
+  // <Hiro />
+  // <br />
+  // <CardApp />
+  // <br />
+  // <MassageAndVi />
+  // <File />
+  // <PepoleMangament />
+  // {/* <Skills/>  */}
+  // <AboutHealth />
+  // <AsNavFor />
 
+  // <OurAdvantages />
+
+  // <SlowerExample />
+
+  // {/* <IphoneScreenCard/> */}
+  // {/* <IphoneScreenShot/> */}
+
+  // <ContactForm />
   return (
     <>
       {/* <div className='px-10 lg:px-0 xl:px-36'> */}
 
-      <div className=" dark:bg-dark-500">
+      <div className=" dark:bg-dark-500 px-6 lg:px-20 xl:px-36">
         <Navbar />
-        {/* <Hiro />
-      <br/>
-      <CardApp/>
-      <Skills />
-      {/* <Honors /> */}
-        {/* <Certs /> */}
-
+        <Hiro />
+        <br />
+        <CardApp />
+        <br />
+        <MassageAndVi />
+        <PepoleMangament />
+        <AboutHealth />
+        <AsNavFor />
+        <OurAdvantages />
+        <SlowerExample />
+        <ContactForm />
+      </div>
+      <div>
         <Switch>
           <Route path="/" exact component={Home} />
 
